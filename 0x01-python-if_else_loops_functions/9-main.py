5d4e49aa6090907dc173de1d240b0914fb4ec540
mastingos <mativokyle@gmail.com>
2024-01-18 01:35:48 -0800

    9

--- 0x01-python-if_else_loops_functions/9-main.py
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -3,5 +3,5 @@ print_last_digit = __import__('9-print_last_digit').print_last_digit
 
 print_last_digit(98)
 print_last_digit(0)
-r = print_last_digit(-1024)
+r = print_last_digit("Holberton")
 print(r)
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if isinstance(number, int):
-        number = str(number)
-        print('{}'.format(number[-1]), end='')
-        return number[-1]
+    number = str(number)
+    print('{}'.format(number[-1]), end='')
+    return number[-1]
