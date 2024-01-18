a71ba416ad2aef97515017fc31fe68ecf0c2dbd1
mastingos <mativokyle@gmail.com>
2024-01-17 22:42:37 -0800

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 result =""
 for i in range(10):
     for j in range(i + 1, 10):
-        result += "{:d}{:d} ,".format(i, j)
+        result += "{:d}{:d}, ".format(i, j)
 result = result.rstrip(', ')
 print(result)
