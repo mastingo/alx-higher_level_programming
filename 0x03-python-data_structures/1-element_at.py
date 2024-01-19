94bfda0f47ddf1eb2e20b8f54910d4363512d014
mastingos <mativokyle@gmail.com>
2024-01-19 04:09:46 -0800

    1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    idx = len(my_list) - 1
     if idx < 0:
-        return none
-    elif idx > len(my_list):
-        return none
+        return None
+    elif idx >= (len(my_list)):
+        return None
     else:
-        return idx
+        return my_list[idx]
