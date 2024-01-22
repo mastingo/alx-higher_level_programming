dcac15acec126bc378259fe482cbd273b64f1735
mastingos <mativokyle@gmail.com>
2024-01-22 00:28:20 -0800

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if sentence != '':
         first = sentence[0]
-            return len(sentence), first
-        else:
-            first = None
-            return len(sentence), first
+        return len(sentence), first
+    else:
+        first = None
+        return len(sentence), first
