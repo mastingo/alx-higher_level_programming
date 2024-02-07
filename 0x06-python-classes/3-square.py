e56606a1b390b9226d5b70236cfd07aba769ab45
mastingo <mativokyle@gmail.com>
2024-02-07 19:22:37 +0300

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,9 @@
 
 
 class Square:
+    """creating an initing a class"""
     def __init__(self, size=0):
+        """ initializes args """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -12,5 +14,6 @@ class Square:
             self.__size = size
 
     def area(self):
+        """method that find area of a square"""
         return self.__size ** 2
 
