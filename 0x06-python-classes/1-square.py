d644e5517cf61b8e47e0aed4523a9da69d863602
mastingo <mativokyle@gmail.com>
2024-02-07 06:54:38 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+"""basing class swuare on prev"""
+class Square:
+    """initializing private instance of attribute size"""
+    def __init__(self):
+        self._size = none
