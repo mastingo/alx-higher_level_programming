6d1dec41357125068a3b8feb15ec4f486d4160ed
mastingo <mativokyle@gmail.com>
2024-02-12 22:45:02 +0300

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,43 +1,48 @@
 #!/usr/bin/python3
-
+"""class rectangle"""
 
 class Rectangle:
+    """init for self"""
 
     def __init__(self, width=0, height=0):
+        """private attributes"""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """getter got priv"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        """setter"""
+        if type(value) != int:
             raise TypeError('width must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
+        """getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        """setter"""
+        if type(value) != int:
             raise TypeError('height must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
-        else:
-            self__height = value
+        self__height = value
 
     def area(self):
+        """area of rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """ of rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        else:
-            return (self.__width * 2) + (self.__height * 2)
+        return (self.__width * 2) + (self.__height * 2)
