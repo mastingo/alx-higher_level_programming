002a61a7f3d0c73dbdc6813dacd2ec00ec98f3e6
mastingo <mativokyle@gmail.com>
2024-02-12 21:23:12 +0300

    1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""class rectangle"""
+
+
+class Rectangle:
+    """initializing"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    """property"""
+    def width(self):
+        return self.__width
+
+    @width.setter
+    """setter"""
+    def width(self, value):
+        """exception for type error"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    """property"""
+    def height(self):
+        return self.__height
+
+    @height.setter
+    """setter"""
+    def height(self, value):
+        """height def"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
