5395b4e75e2b4de74b18e0be724805c04ab987c9
mastingo <mativokyle@gmail.com>
2024-02-13 09:46:22 +0300

    4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -43,10 +43,9 @@ class Rectangle:
 
     def perimeter(self):
         """perimeter calc"""
-        if self__width == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        else:
-            return (self.__height * 2) + (self.__width * 2)
+        return (self.__height * 2) + (self.__width * 2)
 
     def __str__(self):
         """public instance"""
