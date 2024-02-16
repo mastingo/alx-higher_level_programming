48d46881b3d833f4765da3865308025500b9e879
mastingo <mativokyle@gmail.com>
2024-02-16 13:36:08 +0300

    4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' print square function module'''
+
+
+def print_square(size):
+    '''def function'''
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if type(size) is float and size < 0:
+        raise TypeError('size must be an integer')
+    if size > 0:
+        print(('#' * size + '\n') * size, end ='')
