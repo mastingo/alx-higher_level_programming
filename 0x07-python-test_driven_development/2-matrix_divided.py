940c670ff8be48edfb6f7b2875cc39cd3b33a39a
mastingo <mativokyle@gmail.com>
2024-02-16 12:33:05 +0300

    1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    counter = 0
+    for element in matrix:
+        for j in element:
+            if not isinstance(j, int) and not isinstance(j, float):
+                raise TypeError('matrix must be a matrix
+                                (list of lists) of integers / floats')
+            counter += 1
+    if counter != len(matrix[0]) * len(matrix):
+        raise TypeError('Each row of the matrix must have the same size')
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    return [[round(j / div, 2) for j in element] for element in matrix]
