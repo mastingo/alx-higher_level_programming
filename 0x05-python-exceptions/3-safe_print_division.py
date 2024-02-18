ded11fb65bcae8dfacbc45062c415fb020e93f41
mastingo <mativokyle@gmail.com>
2024-02-18 14:01:14 +0300

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,5 +8,5 @@ def safe_print_division(a,b):
         except:
             c = None
         finally:
-            print('inside result {}'.format(c))
+            print('inside result: {}'.format(c))
 
