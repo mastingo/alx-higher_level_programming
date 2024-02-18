079e1925566adf1e8c1cbf886e3d26881b5bb144
mastingo <mativokyle@gmail.com>
2024-02-18 13:59:42 +0300

    3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a,b):
+    if type(a) == int and type(b) == int:
+        try:
+            c = a / b
+            return c
+        except:
+            c = None
+        finally:
+            print('inside result {}'.format(c))
+
