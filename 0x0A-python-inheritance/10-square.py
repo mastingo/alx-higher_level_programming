85b78e7e2c2eaf944a8e8d34a6c466ce032f4abe
mastingo <mativokyle@gmail.com>
2024-02-19 16:46:17 +0300

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""subclass with class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """initializing intances"""
+        self.__size = size
+        if size is True:
+            return self.integer_validator('size', size)
+
+    def area(self):
+        """calling area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """calling str"""
+        return '[Rectangle] {:d}/{:d}'.format(self.__size, self.__size)
