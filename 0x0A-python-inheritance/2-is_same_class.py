a24e1382e8cad882ef47e7ff9a061ca497a2f413
mastingo <mativokyle@gmail.com>
2024-02-19 11:47:27 +0300

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""def is_same_class(obj, a_class):
+    return isinstance (obj, a_class)"""
+
+
+def is_same_class(obj, a_class):
+    """return true if obj is the exact class a_class, otherwise false"""
+    return (type(obj) == a_class)
