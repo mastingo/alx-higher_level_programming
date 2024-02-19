abd9c0c40d02a5af98fda6b897f1bd9815d2098e
mastingo <mativokyle@gmail.com>
2024-02-19 15:19:41 +0300

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""importing"""
+
+
+import importlib
+
+module = importlib.import_module("7-base_geometry")
+
+BaseGeometry = module.BaseGeometry
+"""class rectangle inheriting from basegeom)"""
+
+
+class Rectangle(BaseGeometry):
+    """init private instances"""
+    def __init__(self, width, height):
+        """yes"""
+        self.__width = width
+        self.__height = height
+
+        """using super to access parent function"""
+        super().__init__()
+        if height is True:
+            return self.integer_validator('height', height)
+        if width is True:
+            return self.integer_validator('width', width)
