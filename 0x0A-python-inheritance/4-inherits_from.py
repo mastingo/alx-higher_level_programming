0b8c0ebee91bbeb66e0c28e811298b1ce049c321
mastingo <mativokyle@gmail.com>
2024-02-19 12:19:15 +0300

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """checks if obj is a subclass but not the exact class"""
 
+
 def inherits_from(obj, a_class):
     """funvtional"""
-    if type(obj) == a_class:
+    if isinstance(obj, a_class):
         return False
     elif issubclass(type(obj), a_class):
         return True
