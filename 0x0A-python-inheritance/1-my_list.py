d0fc85b30f1b7c27f3d2e15f5f03ec1520e14b6b
mastingo <mativokyle@gmail.com>
2024-02-19 12:52:40 +0300

    3

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,8 +3,11 @@
 
 
 class MyList(list):
+    """ ok"""
     def __init__(self):
+        """0k boomer"""
         super().__init__()
 
     def print_sorted(self):
+        """boomer"""
         print(sorted(self))
