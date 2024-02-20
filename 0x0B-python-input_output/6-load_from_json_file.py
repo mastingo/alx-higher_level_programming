375b834f5d7002ca7e45755b990dba74ae05c06a
mastingo <mativokyle@gmail.com>
2024-02-20 15:16:21 +0300

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""mixed"""
 
 
 import json
 
+
 def load_from_json_file(filename):
+    """docstring"""
     with open(filename, encoding='utf-8') as f:
         return json.load(f)
 
