e743692db78ced9b4a72e1c1dd9840c7eccbdf60
mastingo <mativokyle@gmail.com>
2024-02-20 12:43:41 +0300

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ writing to file
 
 def write_file(filename="", text=""):
     """docstring"""
-    with open(filename,mode='w', encoding="utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         return f.write(text)
