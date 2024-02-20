8fa5090fd9d5b3e7c227334c51b6fdd02540d681
mastingo <mativokyle@gmail.com>
2024-02-20 12:36:32 +0300

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """yeah"""
 
+
 def read_file(filename=""):
     """more documenting"""
-    with open(filename,encoding='utf-8') as f:
-        print(f.read(),end='')
-
-
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end='')
