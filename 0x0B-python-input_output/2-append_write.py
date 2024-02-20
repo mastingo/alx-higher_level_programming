7ef9004105265097362a68658ada160575b14ce2
mastingo <mativokyle@gmail.com>
2024-02-20 12:38:53 +0300

    1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+"""docstring"""
+
 
 def append_write(filename='', text=''):
+    """docstring to pass checker"""
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
-
