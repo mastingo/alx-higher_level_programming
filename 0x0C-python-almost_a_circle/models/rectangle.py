4bee495fd1780393201983ca68a8bfb3d4579c90
mastingo <mativokyle@gmail.com>
2024-02-26 08:43:26 +0300

    8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,15 +91,21 @@ class Rectangle(Base):
         """__str__ override to return this f string format"""
         return f"[Rectangle]({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"   # noqa
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """assign the variables depending on the args index"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        for arg in args:
+            if arg is not None:
+                if len(args) > 0:
+                    self.id = args[0]
+                if len(args) > 1:
+                    self.width = args[1]
+                if len(args) > 2:
+                    self.height = args[2]
+                if len(args) > 3:
+                    self.x = args[3]
+                if len(args) > 4:
+                    self.y = args[4]
+
+        for key, value in kwargs.items():
+            """handling the keyword args here assigning to the repective"""
+            setattr(self, key, value)
