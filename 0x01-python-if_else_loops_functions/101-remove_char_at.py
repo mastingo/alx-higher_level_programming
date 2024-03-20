977b89a4d47d9d6294e6e9a9c1e24b86bf042ae8
mastingo <mativokyle@gmail.com>
2024-03-20 11:21:18 +0300

    15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''definition for poping charachters'''
+
+def remove_char_at(str, n):
+    new_string = ''
+    for index, char in enumerate(str):
+        if index != n:
+            new_string += char
+    return new_string
